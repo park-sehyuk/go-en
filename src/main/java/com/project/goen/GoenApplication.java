d239e91450290e0a62db61f76c6272b32ee75786
park-sehyuk <tpgur0523@gmail.com>
2026-02-21 13:47:54 +0900

    first commit

---
+++ src/main/java/com/project/goen/GoenApplication.java
@@ -0,0 +1,13 @@
+package com.project.goen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoenApplication.class, args);
+	}
+
+}
